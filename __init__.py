5b86c7539859c6adfde60a2d830fee07a159343a
Om Bhattalwar <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-05-10 23:02:33 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from threading import Thread, Event
 from time import sleep
 
 
+
 class ChatGUI:
     def __init__(self, callback, first_message="welcome to ChatBotAI", terminate="quit"):
         """
